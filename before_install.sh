9e23cbd15dc13428c5fd2ea33fa3caeec886b775
Joas Schilling <coding@schilljs.com>
2017-03-25 17:10:42 +0100

    Use temporary path
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- before_install.sh
+++ before_install.sh
@@ -65,6 +65,6 @@ bash ./core_install.sh $DB
 # install phpunit 5.* for BC on php7
 #
 cd ..
-wget https://raw.githubusercontent.com/nextcloud/travis_ci/master/composer.json
+wget https://raw.githubusercontent.com/nextcloud/travis_ci/fix-phpunit-on-php7/composer.json
 composer install
 cd $WORKDIR
